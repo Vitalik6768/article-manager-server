85f3e2458802cd5bb35bf682f668573cba84138a
vitalik <vitalgreg1988@gmail.com>
2023-06-30 18:23:49 +0300

    simple test

--- routes/__test__/index.test.js
+++ routes/__test__/index.test.js
@@ -4,13 +4,13 @@ const request = require('supertest');
 const app = require('../../app');
 
 
-it('retrieves articles by month and user ID', async () => {
-    const res = await request(app)
-      .get('/articles/user/1')
-      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6InRlc3QxIiwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20iLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2ODgxMzE5MTQsImV4cCI6MTY5NTkwNzkxNH0.vSTBKEH4l9UkfNyts3hZJiuj3tFvx-lLZrP0qv2f7Z0');
-      expect(res.status).toBe(200);  
-      expect(typeof res.body).toBe('object');
-  }, 10000);
+// it('retrieves articles by month and user ID', async () => {
+//     const res = await request(app)
+//       .get('/articles/user/1')
+//       .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6InRlc3QxIiwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20iLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2ODgxMzE5MTQsImV4cCI6MTY5NTkwNzkxNH0.vSTBKEH4l9UkfNyts3hZJiuj3tFvx-lLZrP0qv2f7Z0');
+//       expect(res.status).toBe(200);  
+//       expect(typeof res.body).toBe('object');
+//   }, 10000);
 
 
 
@@ -24,10 +24,10 @@ it('retrieves articles by month and user ID', async () => {
 //   });
 
 
-// describe('test', () => {
-//   it('returns "Hello, world!"', async () => {
-//     const res = await request(app).get('/test');
-//     expect(res.status).toBe(200); // Check if the response status is 200 (OK)
-//     expect(res.text).toBe('hello world'); // Check if the response body is "hello world"
-//   });
-// });
+describe('test', () => {
+  it('returns "Hello, world!"', async () => {
+    const res = await request(app).get('/test');
+    expect(res.status).toBe(200); // Check if the response status is 200 (OK)
+    expect(res.text).toBe('hello world'); // Check if the response body is "hello world"
+  });
+});
